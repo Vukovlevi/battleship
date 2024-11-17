8a366bcb95f2b8bed8c49969abeaaccec7d77b49
Szabó-Vukov Levente <szabo.vukov.levente@gmail.com>
2024-11-17 13:39:59 +0100

    feat: testing state + match start + unexpected ship sending

--- server/tests/e2e_test/e2e_test.go
+++ server/tests/e2e_test/e2e_test.go
@@ -380,8 +380,8 @@ func TestEndToEnd(t *testing.T) {
     time.Sleep(time.Millisecond * 10)
     buf = make([]byte, 256)
     n, err = conn1.Read(buf)
-    assert.Assert(n > 0, "message should be longer than 0 byte when testing ship parsing on conn2", "n", n)
-    assert.Nil(err, "there should be no error on ship parsing on conn2", "err", err)
+    assert.Assert(n > 0, "there should be a match start command after both players sent their ships", "n", n)
+    assert.Nil(err, "there should be no error on receiving match start", "err", err)
     cmdType = buf[tcp.MESSAGE_TYPE_OFFSET: tcp.MESSAGE_TYPE_OFFSET + tcp.MESSAGE_TYPE_SIZE][0]
     data := buf[tcp.HEADER_OFFSET]
     assert.Assert(cmdType == tcp.CommandType.MatchStart, "there should be a match start command conn1", "got cmd type", cmdType)
@@ -389,12 +389,24 @@ func TestEndToEnd(t *testing.T) {
 
     buf = make([]byte, 256)
     n, err = conn2.Read(buf)
-    assert.Assert(n > 0, "message should be longer than 0 byte when testing ship parsing on conn2", "n", n)
-    assert.Nil(err, "there should be no error on ship parsing on conn2", "err", err)
+    assert.Assert(n > 0, "there should be a match start command after both players sent their ships", "n", n)
+    assert.Nil(err, "there should be no error on receiving match start", "err", err)
     cmdType = buf[tcp.MESSAGE_TYPE_OFFSET: tcp.MESSAGE_TYPE_OFFSET + tcp.MESSAGE_TYPE_SIZE][0]
     data = buf[tcp.HEADER_OFFSET]
     assert.Assert(cmdType == tcp.CommandType.MatchStart, "there should be a match start command conn2", "got cmd type", cmdType)
     assert.Assert(data == 0, "player2 should not be the starting one")
+
+    //testing unexpected ships sending
+    conn1.Write(cmd.EncodeToBytes())
+    time.Sleep(time.Millisecond * 10)
+    buf = make([]byte, 256)
+    n, err = conn1.Read(buf)
+    assert.Assert(n > 0, "message should be longer than 0 byte when testing unexpected ships being sent", "n", n)
+    assert.Nil(err, "there should be no error on testing unexpected ships being sent", "err", err)
+    cmdType = buf[tcp.MESSAGE_TYPE_OFFSET: tcp.MESSAGE_TYPE_OFFSET + tcp.MESSAGE_TYPE_SIZE][0]
+    assert.Assert(cmdType == tcp.CommandType.Mismatch, "there should be a mismatch error on testing unexpected ships being sent", "got cmd type", cmdType)
+    data = buf[tcp.HEADER_OFFSET]
+    assert.Assert(data == tcp.CommandTypeMismatchCommand.Data[0], "the mismatch type should be command type mismatch", "got mismatch type", data)
 
 	//testing one player disconnecting
 	conn1.Close()
